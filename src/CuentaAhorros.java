8c39832d254b2e562fbe78feac106e6c709ffde0
Mateo Cisneros <138953180+mateo2408@users.noreply.github.com>
2024-06-16 21:41:35 -0500

    Creacion de cuenta de ahorros

--- src/CuentaAhorros.java
+++ src/CuentaAhorros.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class CuentaAhorros {
+
+    public float Monto;
+
+    public void IngresarMonto(){
+        System.out.println("Ingrese el monto de la cuenta de ahorros");
+        Scanner sc = new Scanner(System.in);
+        Monto = (float) sc.nextDouble();
+    }
+
+    public void VerMonto(){
+        System.out.println("Monto de la cuenta de ahorros" + Monto);
+    }
+
+    public void Retirar(){
+        System.out.println("Ingrese el monto a retirar:");
+        Scanner sc = new Scanner(System.in);
+        float retiro = (float) sc.nextDouble();
+        if (retiro > Monto){
+            System.out.println("No se puede retirar el monto ingresado, saldo insuficiente");
+        } else {
+            Monto = Monto - retiro;
+            System.out.println("Retiro exitoso, saldo actual: " + Monto);
+        }
+    }
 }
